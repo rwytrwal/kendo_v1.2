25782c653e444d22daa922ef5165fb1bd1647325
rwytrwal <wytrwal123@gmail.com>
2016-07-19 09:24:18 +0200

    diala

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 /**
  * Created by rwytrwal on 7/18/2016.
  */
+/**
+ * Created by rwytrwal on 7/18/2016.
+ */
+$(function () {
+    dataSource = new kendo.data.DataSource({
+        transport: {
+            read: {
+                url: "http://jsonplaceholder.typicode.com/posts"
+            }
+        }
+
+    });
+    $("#grid").kendoGrid({
+        dataSource:dataSource
+        ,
+        columns: [
+            //{field: "userId",title:"Human"},
+            {field: "id",title:"id"},
+            {field: "title",title:"title"},
+            {field: "body",title:"body"}
+
+        ]
+
+    });
+    $("#countries").kendoAutoComplete({
+        dataTextField: "title",
+        dataSource: dataSource,
+        filter: "startswith",
+        placeholder: "Select user",
+        separator: ", "
+    });
+});
